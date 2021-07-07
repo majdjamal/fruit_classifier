7495b116e07bc6a78370f55791f21faafb613134
majdjamal <majdj@kth.se>
2021-07-07 14:42:46 +0200

    generate bigger dataset

--- data/mini.py
+++ data/mini.py
@@ -14,14 +14,14 @@ dataset = tfds.as_numpy(dataset)
 
 i = 0
 
-X_train = np.zeros((224, 224, 3, 2500))
-y_train = np.zeros((1, 2500))
+X_train = np.zeros((224, 224, 3, 5000))
+y_train = np.zeros((1, 5000))
 
-X_val = np.zeros((224, 224, 3, 500))
-y_val = np.zeros((1, 500))
+X_val = np.zeros((224, 224, 3, 250))
+y_val = np.zeros((1, 250))
 
-X_test = np.zeros((224,224, 3, 500))
-y_test = np.zeros((1, 500))
+X_test = np.zeros((224,224, 3, 250))
+y_test = np.zeros((1, 250))
 
 
 dim = (224, 224)
@@ -37,7 +37,7 @@ for set in sets:
         img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
         img = cv2.normalize(img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F) # CV_32F: 4-byte floating point (float)
 
-        if ind < 2500:
+        if ind < 5000:
             curr_ind = ind
 
             X_train[:,:,:, curr_ind] = img
@@ -45,14 +45,14 @@ for set in sets:
 
             y_train[:, curr_ind] = lbl
 
-        elif ind < 3000:
-            curr_ind = ind - 2500
+        elif ind < 5250:
+            curr_ind = ind - 5000
             X_val[:,:,:, curr_ind] = img
             y_val[:, curr_ind] = lbl
 
-        elif ind < 3500:
+        elif ind < 5500:
 
-            curr_ind = ind - 3000
+            curr_ind = ind - 5250
             X_test[:,:,:, curr_ind] = img
             y_test[:, curr_ind] = lbl
 
